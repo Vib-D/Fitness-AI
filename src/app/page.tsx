92fd83647611ed0a7afc25f0f2be44c1248f86ce
Vib-D <vibhordass2@gmail.com>
2026-02-12 02:08:27 +0530

    Modified Global CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold text-center mt-10'>Welcome to Fitness AI</h1>
+      <h1 className='text-3xl font-bold text-center mt-10 text-blue-300'>Welcome to Fitness AI</h1>
     </div>
   )
 }
