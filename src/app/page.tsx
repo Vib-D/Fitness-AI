eeaa4d94bc2995a6a74b3b2f5f9f0517a1f8e8b1
Vib-D <vibhordass2@gmail.com>
2026-02-12 23:49:37 +0530

    clerk setup done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { SignInButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div>
-      <h1 className='text-3xl font-bold text-center mt-10 text-blue-300'>Welcome to Fitness AI</h1>
+      <SignInButton />
     </div>
   )
 }
