3c3179c0f018bf3ac84f95cd211197d2ea57c943
Vib-D <vibhordass2@gmail.com>
2026-03-06 01:08:51 +0530

    Structuring navbar and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import ConvexClerkProvider from "@/providers/ConvexClerkProvider";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,10 +29,11 @@ export default function RootLayout({
   return (
     <ConvexClerkProvider>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
+        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+          <Navbar />
           {children}
+          <Footer />
+
         </body>
       </html>
     </ConvexClerkProvider>
